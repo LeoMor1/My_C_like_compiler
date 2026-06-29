70208f2e55a0f3750b2058285e2a5d79b3738cd9
Léo Morin <leo.morinjung@gmail.com>
2026-06-29 18:45:04 +0200

    set-up hello wolrd

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+
+    let 
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs ; [
+          gcc
+          cmake
+          valgrind
+          gdb
+        ];
+
+        buildInputs = with pkgs; [
+
+        ];
+      };
+    };
+}
